418b4709c65d3aaf62d3c2faec0a22e9f6bceb47
Leah Garrison <lgarrison1023@gmail.com>
2021-07-23 10:28:47 -0400

    updated cp .env.example command to switch to the pull repo directory

--- src/commands/deploy.js
+++ src/commands/deploy.js
@@ -38,7 +38,7 @@ module.exports = async () => {
   // need to make sure user is in the cloned project folder; make this a confirmation
   console.log(`Now your Airbyte is all set up. Go to your Tapestry project folder, called ${projectName} for Grouparoo deployment to begin.`); // TODO- change to confirmation
   execSync(`git clone ${githubDeployUrl}`);
-  execSync(`cd ${projectName} && cp .env.example .env `);
+  execSync(`cd ${grouparooDirectory} && cp .env.example .env `);
   connectToECR(grouparooDirectory);
   
 
