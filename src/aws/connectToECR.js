a84a046fad3964d5dc6a837ce9f0e51033a81da5
Katherine Beck <katherine.murphy.beck@gmail.com>
2021-07-24 00:40:41 -0700

    updated ecr and deploy code

--- src/aws/connectToECR.js
+++ src/aws/connectToECR.js
@@ -1,77 +1,83 @@
-const { execSync } = require('child_process');
-const { yamlWriter } = require('../utils/yamlWriter.js');
+const { execSync } = require("child_process");
+const { yamlWriter } = require("../utils/yamlWriter.js");
 
 const getRegion = () => {
   return execSync(`aws configure get region`).toString().trim();
-}
+};
 
 const getAccountId = () => {
-  return  JSON.parse(execSync(`aws sts get-caller-identity`)).Account;
-}
+  return JSON.parse(execSync(`aws sts get-caller-identity`)).Account;
+};
 
-
-
-const connectToECR = (repoName) => {
+const connectToECR = (repoName, randomString) => {
+  console.log("hello!");
   const region = getRegion();
   const accountId = getAccountId();
-    // LOGIN
-    execSync(`aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin "${accountId}.dkr.ecr.${region}.amazonaws.com"`, {stdio:'inherit'});
-   
-    // switch context to run build command
-    // execSync(`sudo service docker start`)
-    execSync(`docker context use default`);
-    // BUILD
-    execSync(`docker build -t grouparoo ./${repoName}`, {stdio: 'inherit'});
-    
-    // // CREATE REPO
-    execSync(`aws ecr create-repository \
+  // LOGIN
+  execSync(
+    `aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin "${accountId}.dkr.ecr.${region}.amazonaws.com"`,
+    { stdio: "inherit" }
+  );
+
+  // switch context to run build command
+  execSync(`docker context use default`);
+  // BUILD
+  execSync(`docker build -t grouparoo ./${repoName}`, { stdio: "inherit" });
+
+  // // CREATE REPO
+  execSync(
+    `aws ecr create-repository \
     --repository-name grouparoo \
     --image-scanning-configuration scanOnPush=true \
-    --region ${region}`, {stdio: 'inherit'});
+    --region ${region}`,
+    { stdio: "inherit" }
+  );
 
+  // // TAG
+  execSync(
+    `docker tag grouparoo:latest ${accountId}.dkr.ecr.${region}.amazonaws.com/grouparoo:latest`,
+    { stdio: "inherit" }
+  );
 
-    // // TAG
-    execSync(`docker tag grouparoo:latest ${accountId}.dkr.ecr.${region}.amazonaws.com/grouparoo:latest`, {stdio: 'inherit'});
-    
-    //   PUSH
-    execSync(`docker push ${accountId}.dkr.ecr.${region}.amazonaws.com/grouparoo:latest`, {stdio: 'inherit'});
-    console.log('pushing')
+  //   PUSH
+  execSync(
+    `docker push ${accountId}.dkr.ecr.${region}.amazonaws.com/grouparoo:latest`,
+    { stdio: "inherit" }
+  );
+  console.log("pushing");
 
-    console.log(`Please select an "Existing AWS Profile" from the following menu, and hit enter. Then select the "default" AWS Profile and hit enter"`);
-    execSync(`docker context create ecs myecscontext21`, {stdio: 'inherit'});
-    execSync(`docker context use myecscontext21`);
-    // const imageUrl = "kmbeck428/docker-grouparoo-test"
-    const imageUrl = `${accountId}.dkr.ecr.${region}.amazonaws.com/grouparoo:latest`;
+  console.log(
+    `Please select an "Existing AWS Profile" from the following menu, and hit enter. Then select the "default" AWS Profile and hit enter"`
+  );
+  execSync(`docker context create ecs myecscontext_${randomString}`, { stdio: "inherit" });
+  execSync(`docker context use myecscontext_${randomString}`);
+  // const imageUrl = "kmbeck428/docker-grouparoo-test"
+  const imageUrl = `${accountId}.dkr.ecr.${region}.amazonaws.com/grouparoo:latest`;
 
-    // writes docker-compose.yml for immediate use
-    yamlWriter(imageUrl);
-    execSync(`docker compose up`, { stdio: 'inherit' });
+  // writes docker-compose.yml for immediate use
+  yamlWriter(imageUrl);
+  execSync(`docker compose up`, { stdio: "inherit" });
 
-    // 
-}
+  //
+};
 // connectToECR();
 
 module.exports = {
-  connectToECR
-}
+  connectToECR,
+};
 
-  // console.log(`Please select an "Existing AWS Profile" from the following menu, and hit enter. Then select the "default" AWS Profile and hit enter"`);
-  // execSync(`docker context create ecs myecscontext13`, {stdio: 'inherit'});
-  // execSync(`docker context use myecscontext13`);
-  // // const imageUrl = "kmbeck428/docker-grouparoo-test"
-  // const imageUrl = `${accountId}.dkr.ecr.${region}.amazonaws.com/grouparoo:latest`;
-  // execSync(`export URL=${imageUrl} && echo $URL && docker compose up`, {stdio: 'inherit'} );
-  // execSync(`${URL}`, {stdio: 'inherit'});
-  // execSync(`docker compose up`);
+// console.log(`Please select an "Existing AWS Profile" from the following menu, and hit enter. Then select the "default" AWS Profile and hit enter"`);
+// execSync(`docker context create ecs myecscontext13`, {stdio: 'inherit'});
+// execSync(`docker context use myecscontext13`);
+// // const imageUrl = "kmbeck428/docker-grouparoo-test"
+// const imageUrl = `${accountId}.dkr.ecr.${region}.amazonaws.com/grouparoo:latest`;
+// execSync(`export URL=${imageUrl} && echo $URL && docker compose up`, {stdio: 'inherit'} );
+// execSync(`${URL}`, {stdio: 'inherit'});
+// execSync(`docker compose up`);
 
-
-// --sk 
+// --sk
 // --skip-keypress
 
-
-
-
 // --from
-
 
 // let password = execSync(`aws ecr get-login-password --region ${region}`).toString();
--- src/commands/deploy.js
+++ src/commands/deploy.js
@@ -1,25 +1,29 @@
-const { execSync, exec } = require('child_process');
-const { getRandomString } = require('../utils/getRandomString.js');
-const { launchPublicDNS } = require('../utils/launchPublicDNS.js'); 
-const { createAirbyteWarehouse } = require('../airbyte/warehouseSetup/createAirbyteWarehouse.js');
-const { createEC2KeyPair } = require('../aws/createEC2KeyPair.js');
-const { createAirbyteStack } = require('../aws/createAirbyteStack.js');
-const { connectInstance } = require('../aws/connectInstance.js');
-const { registerTargets } = require('../aws/registerTargets'); 
-const { storePublicDNS } = require('../aws/storePublicDNS.js');
-const { setupSnowflakeDestination } = require('../airbyte/setupConnections/setupSnowflakeDestination.js'); 
-const githubDeployUrl = "https://github.com/tapestry-pipeline/grouparoo-config-deploy.git";
-const { connectToECR } = require('../aws/connectToECR.js');
-const grouparooDirectory = 'grouparoo-config-deploy';
-
+const { execSync, exec } = require("child_process");
+const { getRandomString } = require("../utils/getRandomString.js");
+const { launchPublicDNS } = require("../utils/launchPublicDNS.js");
+const {
+  createAirbyteWarehouse,
+} = require("../airbyte/warehouseSetup/createAirbyteWarehouse.js");
+const { createEC2KeyPair } = require("../aws/createEC2KeyPair.js");
+const { createAirbyteStack } = require("../aws/createAirbyteStack.js");
+const { connectInstance } = require("../aws/connectInstance.js");
+const { registerTargets } = require("../aws/registerTargets");
+const { storePublicDNS } = require("../aws/storePublicDNS.js");
+const {
+  setupSnowflakeDestination,
+} = require("../airbyte/setupConnections/setupSnowflakeDestination.js");
+const githubDeployUrl =
+  "https://github.com/tapestry-pipeline/deploy-config-grouparoo.git";
+const { connectToECR } = require("../aws/connectToECR.js");
+const grouparooDirectory = "deploy-config-grouparoo";
 
 module.exports = async () => {
   // await createAirbyteWarehouse();
-  
+
   // console.log('Provisioning AWS cloud resources...');
-  const randomString = getRandomString(); 
-  const keyPairName = "tapestry-key-pair" + randomString; 
-  // createEC2KeyPair(keyPairName); 
+  const randomString = getRandomString();
+  const keyPairName = "tapestry-key-pair" + randomString;
+  // createEC2KeyPair(keyPairName);
   // createAirbyteStack(keyPairName);
 
   // console.log('Installing Airbyte on EC2 instance...')
@@ -27,21 +31,28 @@ module.exports = async () => {
 
   // console.log('Registering target to target group...')
   // registerTargets(keyPairName);
-  
+
   // console.log('Launching Airbyte UI to enter login information...')
   // storePublicDNS(keyPairName);
   // const publicDNS = JSON.parse(execSync('aws ssm get-parameter --name "/airbyte/public-dns"').toString()).Parameter.Value;
   // launchPublicDNS(publicDNS);
 
   // await setupSnowflakeDestination('tapestry-key-pair-j4093', publicDNS);
-  
-  const projectName = JSON.parse(execSync('aws ssm get-parameter --name "/project-name"').toString()).Parameter.Value;
-  // need to make sure user is in the cloned project folder; make this a confirmation
-  console.log(`Now your Airbyte is all set up. Go to your Tapestry project folder, called ${projectName} for Grouparoo deployment to begin.`); // TODO- change to confirmation
-  execSync(`git clone ${githubDeployUrl}`);
-  execSync(`cd ${grouparooDirectory} && cp .env.example .env && npm install -g grouparoo && npm install && grouparoo apply`); // TODO- Use FS to make the switch to right folder
-  connectToECR(grouparooDirectory, randomString);
-  
-  console.log('Deployment finished!');
 
-}
+  const projectName = JSON.parse(
+    execSync('aws ssm get-parameter --name "/project-name"').toString()
+  ).Parameter.Value;
+  // need to make sure user is in the cloned project folder; make this a confirmation
+  console.log(
+    `Now your Airbyte is all set up. Go to your Tapestry project folder, called ${projectName} for Grouparoo deployment to begin.`
+  ); // TODO- change to confirmation
+  execSync(`git clone ${githubDeployUrl}`);
+  execSync(`cd ${grouparooDirectory} && npm install`, {
+    stdio: "inherit",
+  });
+  console.log("now getting ready for connecting to ECR...");
+  // npm install -g grouparoo && grouparoo apply // TODO- Use FS to make the switch to right folder
+  connectToECR(grouparooDirectory, randomString);
+
+  console.log("Deployment finished!");
+};
