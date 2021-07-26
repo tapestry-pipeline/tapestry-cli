729321b214324f2f24e9b76c9001e5f18f2e3f93
Leah Garrison <lgarrison1023@gmail.com>
2021-07-26 10:58:20 -0400

    removed kickstartEnvWriter.js and deployEnvWriter.js; empty files. updated .envWriter.js to include creating mailchimp env variables

--- src/utils/envWriter.js
+++ src/utils/envWriter.js
@@ -1,19 +1,27 @@
 const { execSync } = require("child_process");
 const fs = require('fs');
-// require('dotenv').config()
-// const account = process.env.SNOW_HOSTNAME
 
 
-const envWriter = () => {
-  // const host = 'dla27293.us-east-1';
+  
+const envWriter = (repoName) => {
+  const deployRepoName = "deploy-config-grouparoo";
   const snowflakeHostAccount = JSON.parse(execSync('aws ssm get-parameter --name "/snowflake/acct-hostname" --with-decryption').toString()).Parameter.Value;
   const snowflakeAccountUsername = JSON.parse(execSync('aws ssm get-parameter --name "/snowflake/acct-username" --with-decryption').toString()).Parameter.Value;
   const snowflakeAccountPassword = JSON.parse(execSync('aws ssm get-parameter --name "/snowflake/acct-pass" --with-decryption').toString()).Parameter.Value;
 
-  const snowflake = `SNOW_HOSTNAME="${snowflakeHostAccount}"\n` + `SNOW_ACCOUNT_USERNAME="${snowflakeAccountUsername}"\n` + `SNOW_ACCOUNT_PASSWORD=${snowflakeAccountPassword}\n`;
+  let snowflake = `SNOW_HOSTNAME="${snowflakeHostAccount}"\n` + `SNOW_ACCOUNT_USERNAME="${snowflakeAccountUsername}"\n` + `SNOW_ACCOUNT_PASSWORD="${snowflakeAccountPassword}"\n`;
 
-  fs.writeFileSync('.env', snowflake, 'utf8');
+  if (repoName === deployRepoName) {
+    fs.writeFileSync('.env', snowflake, 'utf8');
+  } else {
+    const mailchimpApiKey = JSON.parse(execSync('aws ssm get-parameter --name "/mailchimp/apiKey" --with-decryption').toString()).Parameter.Value;
+    const mailchimpListId = JSON.parse(execSync('aws ssm get-parameter --name "/mailchimp/listId" --with-decryption').toString()).Parameter.Value;
+    let mailChimp = `MAILCHIMP_API_KEY="${mailchimpApiKey}"\nMAILCHIMP_LIST_ID="${mailchimpListId}"`;
+
+    fs.writeFileSync('.env', snowflake + mailChimp, 'utf8');
+  }
 }
+
 
 
 module.exports = {
