cb0dde68fc9e40d54e670255cd9520d70d416fe4
Rick Mole <ramole507@gmail.com>
2021-07-26 11:46:37 -0400

    commented out s3 bucket logic; added grabbing information from params store

--- src/commands/teardown.js
+++ src/commands/teardown.js
@@ -2,8 +2,8 @@ const { execSync, exec } = require("child_process");
 
 const teardown = () => {
   // get stack names
-  const airbyteStackName = execSync('COMMAND HERE'); // TODO
-  const grouparooStackName = execSync('COMMAND HERE'); // TODO
+  const airbyteStackName = JSON.parse(execSync('aws ssm get-parameter --name "/airbyte/stack-name"').toString()).Parameter.Value;
+  const grouparooStackName = JSON.parse(execSync('aws ssm get-parameter --name "/grouparoo/stack-name"').toString()).Parameter.Value;
 
   // delete stacks
   console.log('Deleting Airbyte stack...');
@@ -13,32 +13,33 @@ const teardown = () => {
   console.log('Deleting Grouparoo stack...');
   execSync(`aws cloudformation delete-stack --stack-name ${grouparooStackName}`);
   execSync(`aws cloudformation wait stack-delete-complete --stack-name ${grouparooStackName}`);
-  
+
   // get s3 bucket name (get from params store?)
-  const airbyteS3bucketName = execSync('COMMAND HERE'); // TODO
+  // const airbyteS3bucketName = execSync('COMMAND HERE'); // TODO
 
   // empty s3 bucket
-  console.log('Emptying S3 buckets...');
-  execSync(`aws s3 rm s3://${airbyteS3bucketName} --recursive`);
+  // console.log('Emptying S3 buckets...');
+  // execSync(`aws s3 rm s3://${airbyteS3bucketName} --recursive`);
   // TODO - wait for bucket to empty
 
   // delete s3 bucket
-  console.log('Deleting S3 buckets...');
-  execSync(`aws s3api delete-bucket --bucket ${airbyteS3bucketName}`);
-  execSync(`aws s3api wait bucket-not-exists --bucket ${airbyteS3bucketName}`);
-  console.log('Airbyte S3 bucket deleted!');
+  // console.log('Deleting S3 buckets...');
+  // execSync(`aws s3api delete-bucket --bucket ${airbyteS3bucketName}`);
+  // execSync(`aws s3api wait bucket-not-exists --bucket ${airbyteS3bucketName}`);
+  // console.log('Airbyte S3 bucket deleted!');
+
+  // remove ECR repo
+  console.log('Removing Grouparoo repository from ECR...');
+  execSync('aws ecr delete-repository --repository-name grouparoo');
 
   // removing key-pairs
   console.log('Removing Tapestry key-pairs...');
-  const keyPairName = ''; // from param store?
+  const keyPairName = JSON.parse(execSync('aws ssm get-parameter --name "/tapestry/key-pair-name"').toString()).Parameter.Value;
   execSync(`aws ec2 delete-key-pair --key-name ${keyPairName}`);
 }
 
-
 module.exports = () => {
   console.log('Teardown process initiated...');
-
   teardown();
-
   console.log('Teardown complete!');
 }
