da193d4d1f5e7386c69c984cb3ae0cfa166860da
Leah Garrison <lgarrison1023@gmail.com>
2021-07-29 11:23:39 -0400

    fixed airbyte storeWorkspaceId function. can no longer use default slug

--- src/airbyte/api/storeWorkspaceId.js
+++ src/airbyte/api/storeWorkspaceId.js
@@ -14,14 +14,39 @@ const { execSync } = require('child_process');
 //   }
 // });
 
+
+// async function storeWorkspaceId(domainName) {
+//   return await axios
+//     .post(`${domainName}/api/v1/workspaces/get_by_slug`, { slug: "default" })
+//     .then((response) => {
+//       let data = response.data;
+//       execSync(`aws ssm put-parameter --name "/airbyte/workspace-id" --value "${data.workspaceId}" --type String --overwrite`);
+//     })
+//     .catch((error) => console.log(error));
+// }
+
+// async function getWorkspaceId(domainName) {
+//   return await axios
+//     .post(`${domainName}/api/v1/workspaces/workspaces/list`)
+//     .then((response) => {
+//       let data = response.data;
+//       let workspaceObject = data.workspaces[0];
+//       return workspaceObject.workspaceId
+//     })
+//     .catch((error) => console.log(error));
+    
+// }
+
 async function storeWorkspaceId(domainName) {
   return await axios
-    .post(`${domainName}/api/v1/workspaces/get_by_slug`, { slug: "default" })
-    .then((response) => {
-      let data = response.data;
-      execSync(`aws ssm put-parameter --name "/airbyte/workspace-id" --value "${data.workspaceId}" --type String --overwrite`);
-    })
-    .catch((error) => console.log(error));
+  .post(`${domainName}/api/v1/workspaces/list`)
+  .then((response) => {
+    let data = response.data;
+    let workspaceObject = data.workspaces[0];
+    execSync(`aws ssm put-parameter --name "/airbyte/workspace-id" --value "${workspaceObject.workspaceId}" --type String --overwrite`);
+  })
+  .catch((error) => console.log(error));
+
 }
 
 module.exports = {
