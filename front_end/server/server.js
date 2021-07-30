bd34fe68231672f733f81983628a9708d1aeea4d
Leah Garrison <lgarrison1023@gmail.com>
2021-07-29 20:54:53 -0400

    added front_end folder and updated package.json

---
+++ front_end/server/server.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+// const getFromTable = require(`${__dirname}/../../src/aws/api/dynamoDB/getFromTable`);
+// const configDir = require('../../src/aws/utils/configDir');
+// require('dotenv').config({ path: `${configDir}/.env` });
+const app = express();
+const host = 'localhost';
+const port = 7777;
+
+app.use(cors());
+
+app.use(express.static(path.resolve(__dirname + '/../app/build/')));
+
+app.get('/', (_, res) => {
+  res.sendFile(path.resolve(__dirname + '/../app/build/index.html'));
+});
+
+// app.get('/events', async (req, res) => {
+//   const events = await getFromTable('EVENTS');
+//   events.forEach((event) => (event.ID = event.ID.split('_')[0]));
+//   res.set('Content-Type', 'application/json');
+//   res.send(events);
+// });
+
+// app.get('/traffic', async (req, res) => {
+//   const traffic = await getFromTable('TRAFFIC');
+//   res.set('Content-Type', 'application/json');
+//   res.send(traffic);
+// });
+
+// app.get('/endpoints', async (req, res) => {
+//   const endpoints = await getFromTable('SERVICES_CONFIG');
+//   res.set('Content-Type', 'application/json');
+//   res.send(endpoints);
+// });
+
+// app.get('/subdomain', (req, res) => {
+//   res.set('Content-Type', 'application/json');
+//   res.send(
+//     JSON.stringify('https://' + process.env.AWS_DOMAIN_NAME + '/service?id=')
+//   );
+// });
+
+app.listen(port, host, () => {
+  console.log(`\nYour Tapestry Dashboard is now available at: http://${host}:${port}.`);
+  console.log('Control + C to quit.');
+});
