2442d047fc086931bcb82ba04d730885b841a66f
Rick Mole <ramole507@gmail.com>
2021-07-30 13:54:22 -0400

    fixed timing log for creating an airbyte stack

--- src/aws/createAirbyteStack.js
+++ src/aws/createAirbyteStack.js
@@ -6,7 +6,7 @@ const createAirbyteStack = (projectName, keyPairName, randomString) => {
   const [subnetA, subnetB] = JSON.parse(execSync(`aws ec2 describe-subnets --filters "Name=vpc-id,Values=${VpcId}" --query 'Subnets[*].SubnetId'`).toString());
   const airbyteStackName = `${projectName}-airbyte`;
   
-  log('Creating Airbyte stack... (this will take approximately 3 min)');
+  log('Creating Airbyte stack... (this will take approximately 5 min)');
   execSync(`aws cloudformation create-stack --template-body file://airbyte/airbyte-stack.yaml --stack-name ${airbyteStackName} --parameters ParameterKey=KeyPair,ParameterValue=${keyPairName} ParameterKey=DefaultBucket,ParameterValue=${projectName}-bucket-${randomString} ParameterKey=VpcId,ParameterValue=${VpcId} ParameterKey=VpcSubnetA,ParameterValue=${subnetA} ParameterKey=VpcSubnetB,ParameterValue=${subnetB} --capabilities CAPABILITY_NAMED_IAM`);
   execSync(`aws cloudformation wait stack-create-complete --stack-name ${airbyteStackName}`);
   execSync(`aws ssm put-parameter --name "/airbyte/stack-name" --value "${airbyteStackName}" --type String --overwrite`);
