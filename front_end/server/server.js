6400c3d7e0380cae5f92977af1677229f7c68b89
Leah Garrison <lgarrison1023@gmail.com>
2021-08-01 21:04:09 -0400

    updated css for dbt,snowflake page

--- front_end/server/server.js
+++ front_end/server/server.js
@@ -10,6 +10,7 @@ const { getAirbyteLogs } = require(`${__dirname}/../../src/airbyte/api/getAirbyt
 const { getTables } = require(`${__dirname}/../../src/airbyte/warehouseSetup/getTables.js`); 
 const { getQueryHistory } = require(`${__dirname}/../../src/airbyte/warehouseSetup/getQueryHistory.js`); 
 const { getGrouparooLogs } = require(`${__dirname}/../../src/grouparoo/api/getGrouparooLogs.js`);
+const { getGrouparooDestinations } = require(`${__dirname}/../../src/grouparoo/api/getGrouparooDestinations.js`);
 const app = express();
 const host = 'localhost';
 const port = 7777;
@@ -77,13 +78,16 @@ app.get('/api/airbyte/getlogs', async(req, res) => {
   res.send(data);
 });
 
+const apiKey = "7e64b77f47ca4aa3911bcad16520c8f8"
 app.get('/api/grouparoo/getlogs', async(req, res) => {
-  const apiKey = "7e64b77f47ca4aa3911bcad16520c8f8"
   const dns = JSON.parse(execSync('aws ssm get-parameter --name "/grouparoo/public-dns"').toString()).Parameter.Value;
   let data = await getGrouparooLogs(dns, apiKey);
   res.set('Content-Type', 'application/json');
   res.send(data);
 });
+
+
+
 
 app.get('/api/grouparoo/getdns', async(req, res) => {
   const dns = JSON.parse(execSync('aws ssm get-parameter --name "/grouparoo/public-dns"').toString()).Parameter.Value;
@@ -112,32 +116,33 @@ app.get('/api/grouparoo/cpu', async(req, res) => {
   // const instanceId = getInstanceId(keyPairName); 
   // const cpuUtilization = JSON.parse(execSync(`aws cloudwatch get-metric-statistics --metric-name CPUUtilization --dimensions Name=InstanceId,Value=${instanceId} --start-time ${minusSix} --end-time ${date} --period 900 --statistics Average --namespace AWS/EC2`).toString()).Datapoints; 
   
-  res.set('Content-Type', 'application/json');
-  res.send(cpuUtilization);
+  // res.set('Content-Type', 'application/json');
+  // res.send(cpuUtilization);
 
 })
+
 
 // TODO - grab right grouparoo data
 // aws ecs describe-clusters --clusters test-project
 
 app.get('/api/grouparoo/getcards', async(req, res) => {
-  const dns = JSON.parse(execSync('aws ssm get-parameter --name "/airbyte/public-dns"').toString()).Parameter.Value;
-  const workspaceId = JSON.parse(execSync('aws ssm get-parameter --name "/airbyte/workspace-id"').toString()).Parameter.Value;
-  const keyPairName = JSON.parse(execSync('aws ssm get-parameter --name "/airbyte/key-pair-name"').toString()).Parameter.Value;
-  const instanceId = getInstanceId(keyPairName);
+  const dns = JSON.parse(execSync('aws ssm get-parameter --name "/grouparoo/public-dns"').toString()).Parameter.Value;
+  // const keyPairName = JSON.parse(execSync('aws ssm get-parameter --name "/airbyte/key-pair-name"').toString()).Parameter.Value;
+  // const projectName = JSON.parse(execSync('aws ssm get-parameter --name "/grouparoo/public-dns"').toString()).Parameter.Value;
+  // const instanceId = getInstanceId(keyPairName);
 
-  const count = await countSources(dns, workspaceId); 
-  const instanceData = JSON.parse(execSync(`aws ec2 describe-instance-status --instance-id ${instanceId}`).toString()).InstanceStatuses[0]
-  const state = instanceData.InstanceState.Name;
-  const instanceStatus = instanceData.InstanceStatus.Details[0].Status
-  const systemStatus = instanceData.SystemStatus.Details[0].Status 
+  const count = await getGrouparooDestinations(dns, apiKey); 
+  // const instanceData = JSON.parse(execSync(`aws ec2 describe-instance-status --instance-id ${instanceId}`).toString()).InstanceStatuses[0]
+  // const state = instanceData.InstanceState.Name;
+  // const instanceStatus = instanceData.InstanceStatus.Details[0].Status
+  // const systemStatus = instanceData.SystemStatus.Details[0].Status 
 
   
   const data = [ 
-    {name: "Sources", value: count}, 
-    {name: "EC2 Instance State", value: state}, 
-    {name: "EC2 Instance Reachability", value: instanceStatus}, 
-    {name: "System Reachability", value: systemStatus }
+    {name: "Destinations", value: count}, 
+    // {name: "EC2 Instance State", value: state}, 
+    // {name: "EC2 Instance Reachability", value: instanceStatus}, 
+    // {name: "System Reachability", value: systemStatus }
   ]
     
   res.set('Content-Type', 'application/json');
---
+++ src/grouparoo/api/getGrouparooDestinations.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+const getGrouparooDestinations = async (DNS, apiKey) => {
+  return await axios.get(`${DNS}/api/v1/destinations?&apiKey=${apiKey}`)
+  .then(({data}) => {
+    console.log(data)
+    return data.total
+  })
+  .catch(error => {
+    console.log(error)
+  })
+}
+
+module.exports = {
+  getGrouparooDestinations
+}
